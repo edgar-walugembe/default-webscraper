cb166781bc43fb5da5c66d89c08500bbba5058ea
windows <edgar.walugembe.upti@gmail.com>
2024-06-14 18:20:57 +0300

    web-scraper 0001

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const dataset = await Dataset.open(process.env.DATASET_NAME_KIBANDA);
-
 /**** Scrapping Autotrader Website ****/
 export const router = createPlaywrightRouter();
 
@@ -14,48 +12,61 @@ router.addHandler("DETAIL", async ({ request, page, log }) => {
   log.debug(`Extracting data: ${request.url}`);
 
   const urlParts = request.url.split("/");
-  const carManufacturer = urlParts[4];
+  const carManufacturer = urlParts[4] || "Not Available";
 
-  const carName = await page.locator("h1.hero-title").textContent();
+  const carName =
+    (await page.locator("h1.hero-title").textContent()) || "Not Available";
 
   const pattern = /\b\d{4}\b/;
 
   const match = carName.match(pattern);
   const carYear = match ? match[0] : "No Year Found";
 
-  const carImage = await page.locator("img#mainPhotoModalMd").textContent();
+  const carImage =
+    (await page.locator("img#mainPhotoModalMd").getAttribute("src")) ||
+    "Not Available";
 
-  const carStatus = await page.locator("span#spec-value-1").textContent();
+  const carStatus =
+    (await page.locator("span#spec-value-1").textContent()) || "Not Available";
 
-  const carBodyType = await page.locator("span#spec-value-3").textContent();
+  const carBodyType =
+    (await page.locator("span#spec-value-3").textContent()) || "Not Available";
 
-  const carTrim = await page.locator("span#spec-value-2").textContent();
+  const carTrim =
+    (await page.locator("span#spec-value-2").textContent()) || "Not Available";
 
-  const carEngine = await page.locator("span#spec-value-4").textContent();
+  const carEngine =
+    (await page.locator("span#spec-value-4").textContent()) || "Not Available";
 
-  const carDrive = await page.locator("span#spec-value-7").textContent();
+  const carDrive =
+    (await page.locator("span#spec-value-7").textContent()) || "Not Available";
 
-  const carPrice = await page.locator(".pa-current-asking-price").textContent();
+  const carPrice =
+    (await page.locator(".pa-current-asking-price").textContent()) ||
+    "Not Available";
 
-  const carMileage = await page.locator(".ca-current-mileage").textContent();
+  const carMileage =
+    (await page.locator(".ca-current-mileage").textContent()) ||
+    "Not Available";
 
-  const carDoors = await page.locator("span#spec-value-12").textContent();
+  const carDoors =
+    (await page.locator("span#spec-value-12").textContent()) || "Not Available";
 
-  const carExteriorColor = await page
-    .locator("span#spec-value-9")
-    .textContent();
+  const carExteriorColor =
+    (await page.locator("span#spec-value-9").textContent()) || "Not Available";
 
-  const carInteriorColor = await page
-    .locator("span#spec-value-10")
-    .textContent();
+  const carInteriorColor =
+    (await page.locator("span#spec-value-10").textContent()) || "Not Available";
 
-  const carFuelType = await page.locator("span#spec-value-13").textContent();
+  const carFuelType =
+    (await page.locator("span#spec-value-13").textContent()) || "Not Available";
 
-  const carTransmission = await page.locator("span#spec-value-5").textContent();
+  const carTransmission =
+    (await page.locator("span#spec-value-6").textContent()) || "Not Available"; //should be 6 even in apify
 
-  const carDescription = await page
-    .locator("div#vdp-collapsible-content-text")
-    .textContent();
+  const carDescription =
+    (await page.locator("div#vdp-collapsible-short-text").textContent()) ||
+    "Not Available";
 
   const results = {
     url: request.url,
@@ -80,13 +91,13 @@ router.addHandler("DETAIL", async ({ request, page, log }) => {
   };
 
   log.debug(`Saving data: ${request.url}`);
-  await dataset.pushData(results);
+  await Dataset.pushData(results);
 
   console.log(results);
+  await Dataset.exportToCSV("scrapped-data");
+  await Dataset.exportToJSON("scrapped-data");
 
   /* save results in key_value_stores' folder */
-  // await Dataset.exportToCSV("scrapped-data");
-  // await Dataset.exportToJSON("scrapped-data");
 });
 
 router.addHandler("CATEGORY", async ({ page, enqueueLinks, request, log }) => {
